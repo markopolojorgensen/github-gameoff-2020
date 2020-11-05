37835d0de86df260811f4908e6a8a54f2881ba85
markopolodev <marko.polo.jorgensen@gmail.com>
2020-11-05 16:38:09 -0600

    This is the optimal form movement in a video game can take.

--- player.gd
+++ player.gd
@@ -1,16 +1,37 @@
 extends RigidBody2D
 
+const movement_impulse = 100
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+func _physics_process(delta):
+	var intended_direction = get_intended_direction()
+	
+	if abs(intended_direction.x) > 0.1:
+		# player wants to accelerate, so do so
+		var horizontal_intended_direction = Vector2(intended_direction)
+		horizontal_intended_direction.y = 0
+		apply_central_impulse(horizontal_intended_direction.normalized() * delta * movement_impulse)
+	else:
+		# player doesn't want to accelerate, so slow down
+		var horizontal_linear_velocity = Vector2(linear_velocity)
+		horizontal_linear_velocity.y = 0
+		apply_central_impulse(-1 * horizontal_linear_velocity.normalized() * delta * movement_impulse)
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+func get_intended_direction():
+	var result = Vector2()
+	
+	if Input.is_action_pressed("ui_left"):
+		result += Vector2.LEFT
+	if Input.is_action_pressed("ui_right"):
+		result += Vector2.RIGHT
+	if Input.is_action_pressed("ui_up"):
+		result += Vector2.UP
+	if Input.is_action_pressed("ui_down"):
+		result += Vector2.DOWN
+	
+	return result
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+
+
+
