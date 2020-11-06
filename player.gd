9d17f8e942c7547ab5aef46658054a128c1d18ca
markopolodev <marko.polo.jorgensen@gmail.com>
2020-11-05 19:01:33 -0600

    player movement and jumping

--- player.gd
+++ player.gd
@@ -1,8 +1,30 @@
 extends RigidBody2D
 
-const movement_impulse = 100
+
+const movement_impulse = 300
+const max_horizontal_speed = 100
+
+const initial_jump_impulse = 120
+const continued_jump_impulse = 10
+var rising = false
+
+
+func _process(_delta):
+	var intended_direction = get_intended_direction()
+	if intended_direction.x < -0.5:
+		$animated_sprite.flip_h = true
+	elif intended_direction.x > 0.5:
+		$animated_sprite.flip_h = false
+	
+	if abs(linear_velocity.x) > 5 or intended_direction.length() > 0.5:
+		$animated_sprite.play("run")
+	else:
+		$animated_sprite.play("idle")
+	
+	$lin_vel.text = "%.2f" % linear_velocity.length()
 
 func _physics_process(delta):
+	# movement
 	var intended_direction = get_intended_direction()
 	
 	if abs(intended_direction.x) > 0.1:
@@ -15,7 +37,36 @@ func _physics_process(delta):
 		var horizontal_linear_velocity = Vector2(linear_velocity)
 		horizontal_linear_velocity.y = 0
 		apply_central_impulse(-1 * horizontal_linear_velocity.normalized() * delta * movement_impulse)
-
+	
+	# limit maximum horizontal speed
+	if abs(linear_velocity.x) > max_horizontal_speed:
+		var overspeed = max_horizontal_speed - abs(linear_velocity.x)
+		apply_central_impulse(Vector2(overspeed * sign(linear_velocity.x), 0))
+	
+	
+	# jumping stuff
+	var wants_to_jump = Input.is_action_pressed("ui_accept")
+	$wants_to_jump.text = str(wants_to_jump)
+	
+	# start jump?
+	if not rising and wants_to_jump and $ground_detector.is_colliding():
+		rising = true
+		$rise_timer.start()
+		apply_central_impulse(Vector2.UP * initial_jump_impulse)
+	
+	# keep rising
+	if rising and wants_to_jump:
+		apply_central_impulse(Vector2.UP * continued_jump_impulse * delta)
+	
+	# stop rising
+	# this applies even when we're on the ground, to control weird tilemap bouncing
+	if not wants_to_jump:
+		rising = false
+		$rise_timer.stop()
+		if linear_velocity.y < 0:
+			apply_central_impulse(-1 * Vector2.DOWN * linear_velocity.y)
+	
+	$is_rising.text = str(rising)
 
 func get_intended_direction():
 	var result = Vector2()
@@ -31,7 +82,5 @@ func get_intended_direction():
 	
 	return result
 
-
-
-
-
+func _on_rise_timer_timeout():
+	rising = false
