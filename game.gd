b91638283209955f79db30709c42ed33f725221c
Fourier Transformer <FourierTransformer@users.noreply.github.com>
2020-11-05 21:33:48 -0600

    added a bunhc of assets

--- game.gd
+++ game.gd
@@ -3,7 +3,7 @@ extends Node
 
 func _ready():
 	OS.window_size = Vector2(768, 432)
-	OS.window_position -= OS.window_size * 1/3
+	OS.window_position = OS.window_size * 1/2
 
 func _unhandled_input(event):
 	if event.is_action_pressed("ui_cancel"):
