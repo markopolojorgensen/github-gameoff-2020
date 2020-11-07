ce22d23eaccd690b523557a89984a670d6f82a6e
markopolodev <marko.polo.jorgensen@gmail.com>
2020-11-06 18:13:16 -0600

    updated window position on startup shenanigans, hopefully this works?

--- game.gd
+++ game.gd
@@ -3,9 +3,16 @@ extends Node
 
 func _ready():
 	OS.window_size = Vector2(768, 432)
-	OS.window_position = OS.window_size * 1/2
+	if OS.get_name() == "X11":
+		OS.window_position -= OS.window_size * 1/3
+	else:
+		OS.window_position = OS.window_size * 1/2
 
 func _unhandled_input(event):
 	if event.is_action_pressed("ui_cancel"):
 		get_tree().quit()
+
+
+
+
 
