af8a35d83f91451133af9dbe4db8978739d6d8cf
markopolodev <marko.polo.jorgensen@gmail.com>
2020-11-13 14:47:49 -0600

    super mario world style camera anchor movement

--- camera.gd
+++ camera.gd
@@ -1,11 +1,39 @@
 extends Camera2D
 
+const speed = 200
 
-func _process(_delta):
+var moving_right = true
+# global coordinates
+var desired_position := Vector2()
+
+func _process(delta):
 	if global.camera_follow:
-		global_position = global.camera_follow.global_position
+		# coordinates relative to top-left corner of screen
+		var target = (global.camera_follow as CanvasItem).get_global_transform_with_canvas().origin
+		if moving_right:
+			var anchor_to_target = target.x - $right_anchor.get_global_transform_with_canvas().origin.x
+			if anchor_to_target > 0:
+				# target is to the right of anchor, catch up!
+				desired_position.x = global_position.x + anchor_to_target
+			elif $left_threshold.get_global_transform_with_canvas().origin.x > target.x:
+				# player passed the left threshold, switch directions
+				moving_right = false
+		else:
+			var anchor_to_target = target.x - $left_anchor.get_global_transform_with_canvas().origin.x
+			if anchor_to_target < 0:
+				# target is to the left of anchor, catch up!
+				desired_position.x = global_position.x + anchor_to_target
+			elif $right_threshold.get_global_transform_with_canvas().origin.x < target.x:
+				# player passed the right threshold, switch directions
+				moving_right = true
+		desired_position.y = global.camera_follow.global_position.y
 	else:
 		print("nothing for camera to follow!")
+	
+	if global_position.distance_to(desired_position) < (speed * delta):
+		global_position = desired_position
+	else:
+		global_position += global_position.direction_to(desired_position) * speed * delta
 	
 
 
