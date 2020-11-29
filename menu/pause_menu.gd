8a85835bb21a55140264c94ce205b931c923a0a8
Fourier Transformer <FourierTransformer@users.noreply.github.com>
2020-11-28 23:47:20 -0600

    pause menu now has a help menu pop up thingy

--- menu/pause_menu.gd
+++ menu/pause_menu.gd
@@ -7,26 +7,31 @@ func _ready():
 	$control/panel_container/v_box_container/continue_container/menu_item.connect("picked", self, "option_picked")
 	$control/panel_container/v_box_container/restart_room_container/menu_item.connect("picked", self, "option_picked")
 	$control/panel_container/v_box_container/restart_level_container/menu_item.connect("picked", self, "option_picked")
+	$control/panel_container/v_box_container/control_help_container/menu_item.connect("picked", self, "option_picked")
 	$control/panel_container/v_box_container/exit_to_menu/menu_item.connect("picked", self, "option_picked")
 	$control/panel_container/v_box_container/quit_to_windows/menu_item.connect("picked", self, "option_picked")
 
 func option_picked(option):
 	match option:
 		"continue":
-			pass
+			continue_game()
 		"restart_room":
 			global.current_room.respawn_player_in_last_room(global.player)
+			continue_game()
 		"restart_level":
 			global.level_manager.load_level()
+			continue_game()
 		"to_main_menu":
 			print("TODO: I'm not sure how to go to the main menu...")			
 			pass
+		"controls":
+			print("test")
+			$help_menu.get_node("control").visible = !$help_menu.get_node("control").visible
 		"quit":
 			get_tree().quit()
 		_:
 			print("mystery menu option: %s" % option)
 			
-	continue_game()
 
 func continue_game():
 	get_tree().paused = false
