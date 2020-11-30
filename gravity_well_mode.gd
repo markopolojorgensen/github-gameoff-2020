894eb144b1d813b4c6352300568263d751feb53f
markopolodev <marko.polo.jorgensen@gmail.com>
2020-11-30 13:08:10 -0600

    gravity well cursor improvements

--- gravity_well_mode.gd
+++ gravity_well_mode.gd
@@ -131,12 +131,12 @@ func _on_ground_detector_body_exited(_body):
 func update_cursor():
 	if $ground_detector.get_overlapping_bodies().size() <= 0 and gravity_well_tracker.can_add_well():
 		$sprite.modulate = Color(1, 1, 1, 1)
-		$sprite.play()
+		$sprite.play("default")
 	else:
 		# colliding with ground or out of wells
 		# either way, darken cursor
 		$sprite.modulate = Color(0.5, 0.5, 0.5, 1)
-		$sprite.stop()
+		$sprite.play("borked")
 		$sprite.frame = 0
 
 
